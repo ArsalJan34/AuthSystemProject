5462d59fdf64bde06b46a401bb4642b832d35cf9
ArsalJan34 <arsaljaan532@gmail.com>
2026-01-29 18:03:13 +0500

    Initial Commit

---
+++ login.js
@@ -0,0 +1,23 @@
+
+    function login() {
+      const email = document.getElementById('email').value.trim();
+      const password = document.getElementById('password').value.trim();
+
+      auth.signInWithEmailAndPassword(email, password)
+        .then((cred) => {
+          console.log("Logged in:", cred.user.email);
+
+          db.collection('BakeryCustomers').doc(cred.user.uid).get()
+            .then(doc => {
+              if (doc.exists && doc.data().isAdmin === true) {
+                window.location.href = "admin.html";
+              } else {
+                window.location.href = "store.html";
+              }
+            });
+        })
+        .catch((error) => {
+          console.error(error.code, error.message);
+          alert(error.message); // SHOW REAL ERROR
+        });
+    }
